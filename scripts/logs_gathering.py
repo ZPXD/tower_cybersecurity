a8a7a84673c58309b91a2f5d2c913987d4e21160
ZPXD <zajeciaprogramowaniaxd@gmail.com>
2022-05-20 11:45:14 +0200

    update

--- scripts/logs_gathering.py
+++ scripts/logs_gathering.py
@@ -39,6 +39,7 @@ logs_gathering = {
 		'nginx_access': {'path' : '/var/log/nginx/access.log',},
 		'nginx_error' : {'path' : '/var/log/nginx/error.log',},
 		'exim4' : 		{'path' : '/var/log/exim4/mainlog',},
+		# TBD ...
 	},
 
 	'command_logs' : {
@@ -50,6 +51,7 @@ logs_gathering = {
 		'last' : 		 {'command' : 'last', },
 		'lastb' : 		 {'command' : 'lastb', },
 		'utmp' : 		 {'command' : 'utmpdump /var/run/utmp', },
+		# TBD ...
 	},
 	
 	'log_places' : [
@@ -81,7 +83,8 @@ class LogsGathering():
 	place_for_logs_folder = '/home/{}'
 	logs_gathering_file = 'logs_gathering.json'
 	logs_folder = 'logs_gathering'
-	guardians = 'i_see_my_files.py'
+	i_see_my_files = 'i_see_my_files.py'
+	guardians_refresh = 'guardians_refresh.py'
 	
 	def __init__(self, logs_gathering):
 		self.place_for_logs_folder = self.place_for_logs_folder.format(os.getlogin())
@@ -101,28 +104,30 @@ class LogsGathering():
 	def prepare_files_and_commands(self):
 		'''
 		Prepares logs to be scrapped.
+		
+		To potem.
 		'''
-		os.system('sudo unxz /var/log/apt/eipp.log.xz') # Unpacks eipp log into readable format.
-		os.system('export HISTTIMEFORMAT="%F %T" ""') # Adds datestamp to 'history' command.
+		#os.system('sudo unxz /var/log/apt/eipp.log.xz') # Unpacks eipp log into readable format.
+		#os.system('export HISTTIMEFORMAT="%F %T" ""') # Adds datestamp to 'history' command.
 
 	def prepare_places(self):
 		'''
 		Prepare place for logs (folders).
 		'''
-		if not logs_folder in os.listdir(place_for_logs_folder):
-			os.mkdir(logs_folder_path)
-		log_folders = os.listdir(logs_folder_path)
-		for k, v in logs_gathering['file_logs'].items():
+		if not self.logs_folder in os.listdir(self.place_for_logs_folder):
+			os.mkdir(self.logs_folder_path)
+		log_folders = os.listdir(self.logs_folder_path)
+		for k, v in self.logs_gathering['file_logs'].items():
 			if not k in log_folders:
-				save_path = os.path.join(logs_folder_path, k)
+				save_path = os.path.join(self.logs_folder_path, k)
 				os.mkdir(save_path)
-				save_path = os.path.join(logs_folder_path, k, 'history')
+				save_path = os.path.join(self.logs_folder_path, k, 'history')
 				os.mkdir(save_path)
-		for k, v in logs_gathering['command_logs'].items():
+		for k, v in self.logs_gathering['command_logs'].items():
 			if not k in log_folders:
-				save_path = os.path.join(logs_folder_path, k)
+				save_path = os.path.join(self.logs_folder_path, k)
 				os.mkdir(save_path)
-				save_path = os.path.join(logs_folder_path, k, 'history')
+				save_path = os.path.join(self.logs_folder_path, k, 'history')
 				os.mkdir(save_path)
 
 
@@ -148,11 +153,11 @@ class LogsGathering():
 		with open(self.logs_gathering_file_path, 'w') as fp:
 			json.dump(self.logs_gathering, fp)
 
-	def search_add_new_logs(ask=True):
+	def search_add_new_logs(self, ask=True):
 		'''
 		Searches for log files.
 
-		Adds them to logs_gathering config file.
+		Adds them to logs_gathering config file if you decide so.
 
 		ask: bool: default True, asks before adding log to config.
 		'''
@@ -162,18 +167,18 @@ class LogsGathering():
 			existing_log_paths.append((v['path']))
 		for place in self.logs_gathering['log_places']:
 			logs_paths = self.get_all_folder_files(place, 'log')
-			for log_path in log_paths:
+			for log_path in logs_paths: # :)
 				if log_path not in existing_log_paths:
 					if not log_path.endswith('gz') and not log_path[-1].isdigit():
-						name = file.split('.')[0]
+						name = log_path.split('.')[0]
 						new_logs.append((name, log_path))
 
 		print('Found {} new log files:'.format(len(new_logs)))
-		for name, log_path in log_paths:
+		for name, log_path in new_logs:
 			print(name, "-", log_path)
 			if ask:
 				decide = input('Press y to add log.').lower()
-				if decide == 'y'
+				if decide == 'y':
 					logs_gathering['file_logs'][name] = {'path' : log_path}
 			else:
 				logs_gathering['file_logs'][name] = {'path' : log_path}
@@ -209,18 +214,17 @@ class LogsGathering():
 
 				for file in files_in:
 					if condition:
-						if isinstance.(condition, list):
+						if isinstance(condition, list):
 							for c in condition:
 								if c in file:
 									file_path = folder + '/' + file
 									if not file_path in new_files:
 										new_files.append(file_path)
 						else:
-							else:
-								if condition in file:
-									file_path = folder + '/' + file
-									if not file_path in new_files:
-										new_files.append(file_path)
+							if condition in file:
+								file_path = folder + '/' + file
+								if not file_path in new_files:
+									new_files.append(file_path)
 					else:
 						file_path = folder + '/' + file
 						if not file_path in new_files:
@@ -254,16 +258,19 @@ class LogsGathering():
 		Save ALL logs - quick unaccurate method to gather all logs to history folders.
 		'''
 		for name, log in self.logs_gathering['command_logs'].items():
-			file_name = date()
-			save_as = os.path.join(logs_folder_path, 'history', file_name)
+			file_name = self.date()
+			save_as = os.path.join(self.logs_folder_path, 'history', file_name)
 			command = '{} > {}'.format(log['command'], save_as)
 			os.system(command)
 
 		for name, log in self.logs_gathering['file_logs'].items():
-			file_name = date()
-			save_as = os.path.join(logs_folder_path, name, 'history', file_name)
+			file_name = self.date()
+			save_as = os.path.join(self.logs_folder_path, name, 'history', file_name)
 			command = 'cat {} > {}'.format(log['path'], save_as)
 			os.system(command)
+
+	def 
+
 
 	def guardian(self):
 		'''
@@ -281,7 +288,7 @@ class LogsGathering():
 		os.system(command)
 		return command
 
-	def ongoing_bash(self, every_n_seconds=1, script_path, arg_str=''):
+	def ongoing_bash(self, script_path, every_n_seconds=1, arg_str=''):
 		'''
 		.
 		'''
@@ -322,11 +329,18 @@ class LogsGathering():
 			os.system('sudo systemctl daemon-reload')
 			os.system('sudo systemctl enable {}'.format(file_name))
 
+# To do:
+# - history/history
+# - dla guardianow - sprawdzanie procesow po usunieciu pliku jeszcze
 
 if __name__ == "__main__":
-	LOG = LogsGathering(config_path)
-
+	LOG = LogsGathering(logs_gathering)
 
 	LOG.install_prequisites()
 	LOG.prepare_files_and_commands()
 	LOG.prepare_places()
+
+	# Żyjący program robimy, bez default configa
+	#LOG.load_logs_gathering_config_from_file()
+	LOG.search_add_new_logs(ask=True) # ...
+	LOG.quick_logs_gatherer()
